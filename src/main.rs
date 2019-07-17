b16df507af73b5912c58dea95c22459008708524
minaduking <minaduki.supernova@gmail.com>
2019-07-18 02:55:54 +0900

    tcp_server部分を追加

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use std::env;
 #[macro_use]
 extern crate log;
 
+//mod tcp_client;
+mod tcp_server;
+//mod udp_client;
+//mod udp_server;
+
 fn main() {
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
@@ -16,7 +21,7 @@ fn main() {
     match protocol {
         "tcp" => match role {
             "server" => {
-
+                tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
             }
             "client" => {
 
